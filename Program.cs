62e101f06b8d09d63bf84fbbbc359b2913ec8b36
mhrnnoi <mehrannois20@gmail.com>
2022-08-28 13:41:14 +0430

    test : create a sensored string gen

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace censored_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
